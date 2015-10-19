50efa3b62ec60a99c32cd362135c9abfa4a31821
Hans Sebastian <hsebastian@users.noreply.github.com>
2015-10-18 22:43:55 -0700

    Worked on MovieDetail screen

--- PopularMoviesVol1/app/src/main/java/com/example/hsebastian/popularmoviesvol1/MovieDetailActivityFragment.java
+++ PopularMoviesVol1/app/src/main/java/com/example/hsebastian/popularmoviesvol1/MovieDetailActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,13 @@ public class MovieDetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_movie_detail, container, false);
+        View rootView =  inflater.inflate(
+            R.layout.fragment_movie_detail, container, false);
+
+        ImageView imageView = (ImageView) rootView.findViewById(
+            R.id.movie_poster_image_view);
+        imageView.setImageResource(R.drawable.sample_6);
+
+        return rootView;
     }
 }
